70a68b5bdd206bc3878a2f1392f5f9edeaf868f4
Ian Hunter <ian.hunter@sjsu.edu>
2016-03-07 14:29:59 -0800

    deleted img1 variable

---
+++ android/src/com/tomf/myfirstsgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tomf.myfirstsgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tomf.myfirstsgame.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
